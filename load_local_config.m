2aee97e59adb64a9e15d36ac6c37be33c1baf9fa
Daniel Sperka <djsperka@ucdavis.edu>
2024-03-14 16:42:47 -0700

    Update input structs.

--- load_local_config.m
+++ load_local_config.m
@@ -3,11 +3,17 @@ function [cclab] = load_local_config()
 %   Detailed explanation goes here
 
 
-    cclab.dummymode_EYE = 1;
+    cclab.dummymode_EYE = 0;
     cclab.SkipSyncTests = 1;
     cclab.Verbosity = 0;
     cclab.VisualDebugLevel = 1;
-    
+
+    cclab.FixationTime = 0.5;
+    cclab.MaxAcquisitionTime = 2.0;
+    cclab.FixationBreakEarlyTime = 0.5;
+    cclab.FixationBreakLateTime = 2.0;
+
+
     % If 'Fovx' is defined, these two measurements are ignored, and don't
     % need to be present at all but don't hurt. Otherwise, make sure they
     % are correct for your experimental setup and screen. They should be in
@@ -20,13 +26,12 @@ function [cclab] = load_local_config()
     FName = {'N51';'N52';'N53';'N54';'N55';'N56';'N57';'N58';'N59';'N60'};
     Change = [1;2;1;2;0;1;2;1;2;0];
     ChangeContrast = [.5;.5;.5;.5;.5;.5;.5;.5;.5;.5];
-    FixTime  = [1;1;1;1;1;1;1;1;1;1];
     SampTime = [2;2;2;2;2;2;2;2;2;2];
-    GapTime = [1;1;1;1;1;1;1;1;1;1];
+    GapTime = [0;0;0;0;0;0;0;0;0;0];
     TestTime = [2;2;2;2;2;2;2;2;2;2];
     RespTime = [2;2;2;2;2;2;2;2;2;2];
     
-    cclab.trials = table(Type1, Type2, FName, Change, ChangeContrast, FixTime, SampTime, GapTime, TestTime, RespTime);
+    cclab.trials = table(Type1, Type2, FName, Change, ChangeContrast, SampTime, GapTime, TestTime, RespTime);
     cclab.ITI = 1.0;
 end
 
