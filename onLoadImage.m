4a69a8cb02c6a8b1fccedd5051da19ebd187395a
Daniel Sperka <djsperka@ucdavis.edu>
2024-03-19 16:58:53 -0700

    Initial add. Function to process images on load.

---
+++ onLoadImage.m
@@ -0,0 +1,11 @@
+function [img] = onLoadImage(inputImage)
+%onLoadImage Convert to grayscale, clamp to range [clampRegionSize, 
+% 255-clampRegionSize]
+%   Detailed explanation goes here
+    clampRegionSize = 30;
+    img2 = histeq(inputImage);
+    img = img2;
+    img(img2<clampRegionSize) = clampRegionSize;
+    img(img2>(256-clampRegionSize)) = 256-clampRegionSize;
+end
+
