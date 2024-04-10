a88a18dc859fb8a4fd197a6b93de00b249c639c2
Daniel Sperka <djsperka@ucdavis.edu>
2024-04-10 10:25:39 -0700

    Modify for bw images - they have ghad histeq done already. Don't use this on color images!

--- onLoadImage.m
+++ onLoadImage.m
@@ -3,8 +3,9 @@ function [img] = onLoadImage(inputImage)
 % 255-clampRegionSize]
 %   Detailed explanation goes here
     clampRegionSize = 30;
-    img2 = histeq(inputImage);
-    img = img2;
+    %img2 = histeq(inputImage);
+    img = inputImage;
+    img2 = img;
     img(img2<clampRegionSize) = clampRegionSize;
     img(img2>(256-clampRegionSize)) = 256-clampRegionSize;
 end
