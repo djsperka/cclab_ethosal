6e3836c6427c415be2254a9d70f6b82b55c4c4a5
Daniel Sperka <djsperka@ucdavis.edu>
2024-04-17 12:41:49 -0700

    First and second image can have an applied function.

--- showStim12.m
+++ showStim12.m
@@ -1,11 +1,19 @@
-function showStim12(w, imgset, keys, img2Func, t1, tGap, t2)
-%showStim12 display stim as in etholog
-%   For each key in keys, display it, then display it again using img2Func.
+function showStim12(w, imgset, keys, img1Func, img2Func, t1, tGap, t2)
+%showStim12(w, imgset, keys, img2Func, t1, tGap, t2) display stim as in
+%etholog trials.
+%   For each key in keys (celll array), display it using img1Func 
+%   (function_handle or []), then display it again using img2Func.
 %   The values t1, tGap, and t2 are the times for stim1, gap, stim2, in 
 %   seconds. 
 
+    if isempty(img1Func)
+        img1Func = @deal;
+    end
+    if isempty(img2Func)
+        img2Func = @deal;
+    end
     for i=1:length(keys)
-        imgset.flip(w, keys{i});
+        imgset.flip(w, keys{i}, img1Func);
         WaitSecs(t1);
         Screen('Flip', w);
         WaitSecs(tGap);
