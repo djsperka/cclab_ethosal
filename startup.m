de40a52ab176bbd09cf934c43fb7ed6b5b13bd20
Daniel Sperka <djsperka@ucdavis.edu>
2024-04-17 12:43:07 -0700

    Deleted

--- startup.m
+++
@@ -1,6 +0,0 @@
-function startup
-%    fprintf('Adding current folder, and ../cclab-matlab-tools to your path.\n');
-%    addpath(cd);
-%    addpath(fullfile(cd, '../cclab-matlab-tools'));
-%    addpath(fullfile(cd, '../cclab-matlab-tools/demo'));
-end
