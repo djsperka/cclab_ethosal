0515eadf474723e065be833a83037046d80278c3
Daniel Sperka <djsperka@ucdavis.edu>
2024-04-17 12:42:50 -0700

    Get rid of this file.

--- startup.m
+++ startup.m
@@ -1,6 +1,6 @@
 function startup
-   fprintf('Adding current folder, and ../cclab-matlab-tools to your path.\n');
-   addpath(cd);
-   addpath(fullfile(cd, '../cclab-matlab-tools'));
-   addpath(fullfile(cd, '../cclab-matlab-tools/demo'));
+%    fprintf('Adding current folder, and ../cclab-matlab-tools to your path.\n');
+%    addpath(cd);
+%    addpath(fullfile(cd, '../cclab-matlab-tools'));
+%    addpath(fullfile(cd, '../cclab-matlab-tools/demo'));
 end
