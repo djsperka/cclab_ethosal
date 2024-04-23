2861f153e0bd5c6e493469c4baea4e89d140dad3
Daniel Sperka <djsperka@ucdavis.edu>
2024-04-23 16:51:08 -0700

    WIP

--- preamble.m
+++ preamble.m
@@ -10,18 +10,24 @@ classdef preamble < handle
         CharHeight
         TextSize
         OldTextSize
+        Responder
+        Imageset
     end
 
     methods
-        function obj = preamble(cmds, textSize)
+        function obj = preamble(cmds, textSize, resp, img)
             %UNTITLED4 Construct an instance of this class
             %   Detailed explanation goes here
             arguments
                 cmds (:,2) cell
                 textSize (1,1) {mustBeInteger}
+                resp responder
+                img imageset
             end
             obj.Script = cmds;
             obj.TextSize = textSize;
+            obj.Responder = resp;
+            obj.Imageset = img;
         end
 
         function play(obj,w)
@@ -48,6 +54,21 @@ classdef preamble < handle
             Screen('TextSize', w, obj.OldTextSize);
         end
 
+        function [ok] = wait_for_button(obj, which_buttons, max_time_secs)
+            ok = false;
+            startTime = GetSecs;
+            obj.Responder.start();
+            while GetSecs - startTime < max_time_secs
+                [isResp, resp] = obj.Responder.response();
+                if isResp && any(resp==which_buttons)
+                    ok=true;
+                    break;
+                end
+                WaitSecs(0.5);
+            end
+            obj.Responder.stop(true);
+        end
+
         function do_cmd(obj, w, cmd, args)
             switch(cmd)
                 case 'split_screen'
@@ -67,12 +88,10 @@ classdef preamble < handle
                         obj.TextRect = rect;
                         obj.TextRect(2) = obj.TextRect(2)+yfrac;
                     end
-                case 'draw'
-                    if isa(args, 'function_handle')
-                        args(obj.DrawRect);
-                    else
-                        warning(sprintf('Arg to draw should be a function_handle, i=%d', i));
-                    end
+                case 'mkey'
+                    obj.mkey(w, obj.DrawRect, args{:});
+                case 'image'
+                    obj.image(w, obj.DrawRect, args{:});
                 case 'text'
                     DrawFormattedText(w, args, 'center', obj.TextRect(2) + obj.CharHeight, [0,0,0]);
                 case 'flip'
@@ -80,9 +99,57 @@ classdef preamble < handle
                     WaitSecs(args);
                 case 'clear_screen'
                     Screen('Flip', w);
+                case 'wait_button'
+                    obj.wait_for_button(args, 30);
                 otherwise
                     fprintf(1, 'Command %s not implemented\n', cmd);
             end
         end
     end
+
+    methods (Access = private)
+
+        function mkey(obj, w, rect, square_fraction, buttonColors)
+        %drawMKey Draw millikey in center of rectangle.
+        %   Detailed explanation goes here
+
+            arguments
+                obj (1,1) preamble
+                w (1,1) {mustBeInteger}
+                rect (1,4) 
+                square_fraction (1,1) {mustBeFloat(square_fraction), mustBeInRange(square_fraction, 0, 1)} = 1
+                buttonColors (3,5) = ones(3,5)
+            end
+            [rectCenterX, rectCenterY] = RectCenter(rect);
+        
+            % Determine largest square that will fit....
+            if RectWidth(rect) > RectHeight(rect)
+                d = RectHeight(rect) * square_fraction;
+            else
+                d = RectWidth(rect) * square_fraction;
+            end
+            useThisRect = CenterRectOnPoint([0, 0, d, d], rectCenterX, rectCenterY);
+        
+            rowHeight = RectHeight(useThisRect)/3;
+            qWidth = RectWidth(useThisRect)/4;
+            boxRect = SetRect(useThisRect(1), useThisRect(2) + rowHeight, useThisRect(3), useThisRect(4));
+            buttonRect = [0, 0, qWidth/2, qWidth/2];
+            buttonX = useThisRect(1) + [ qWidth, 2*qWidth, 3*qWidth, 1.5*qWidth, 2.5*qWidth]';
+            buttonY = useThisRect(2) + [1.5*rowHeight, 1.5*rowHeight, 1.5*rowHeight, 2.5*rowHeight, 2.5*(rowHeight)]';
+            buttonRects = CenterRectOnPoint(buttonRect, buttonX, buttonY);
+        
+            Screen('FillRect', w, 0, boxRect);
+            Screen('DrawLine', w, 0, rectCenterX, useThisRect(2) + rowHeight, rectCenterX, useThisRect(2), 4);
+            Screen('FillOval', w, buttonColors, buttonRects');
+        
+        end
+
+        function image(obj, w, rect, varargin)
+            t=obj.Imageset.texture(w, varargin{:});
+            [x, y] = RectCenter(obj.DrawRect);
+            Screen('DrawTexture', w, t, [], CenterRectOnPoint(obj.Imageset.UniformOrFirstRect, x, y));
+        end
+
+    end
+
 end
