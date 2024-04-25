d38db5c97a76f7b64b64bb4e9372ee62e0206394
Daniel Sperka <djsperka@ucdavis.edu>
2024-04-25 16:10:00 -0700

    more instructions for generating trials, running etholog

--- run_etholog.m
+++ run_etholog.m
@@ -1,10 +1,23 @@
+% input folder
+input_folder = '/home/cclab/Desktop/ethosal/input';
+output_folder = '/home/cclab/Desktop/ethosal/output';
+
+% % generate trials - 60 images
+% [blocks,inputArgs,parsedResults]=generateEthBlocks(img.BalancedFileKeys, [30,10,20;10,30,20;20,20,20]', .78, .22);
+% 
+% % generate trials - 30 images
+% [blocks,inputArgs,parsedResults]=generateEthBlocks(img.BalancedFileKeys, [15,5,10;5,15,10;10,10,10]', .78, .22);
+% save(fullfile(output_folder, 'contrast_30images_a.mat'), 'blocks', 'inputArgs', 'parsedResults');
+% 
+% save(NAME_HERE, 'blocks', 'inputArgs', 'parsedResults');
+
 % Screen
 PsychDefaultSetup(2);
 [w,wr]=PsychImaging('OpenWindow', 1, [.5 .5 .5]');
 
 % load imageset
 img=imageset('/data/cclab/images/Babies', 'Subfolders', {'H', 'bw'; 'L', 'bw-texture'}, 'OnLoad', @deal);
-cc=load('/home/cclab/Desktop/ethosal/input/contrast_60images_a.mat');
+cc=load('/home/cclab/Desktop/ethosal/input/contrast_30images_a.mat');
 
 
 % parameters needed
