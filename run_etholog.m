305bab3da4165ecb076e9dcf4ecb83f4688c32b7
Daniel Sperka <djsperka@ucdavis.edu>
2024-05-15 11:10:46 -0700

    Make simpler, two-argument function

--- run_etholog.m
+++ run_etholog.m
@@ -1,44 +1,76 @@
-% input folder
-input_folder = '/home/cclab/Desktop/ethosal/input';
-output_folder = '/home/cclab/Desktop/ethosal/output';
+function results = run_etholog(identifier, lrn)
+%run_etholog Run etholog for a specific block
+%   Detailed explanation goes here
 
-% % generate trials - 60 images
-% [blocks,inputArgs,parsedResults]=generateEthBlocks(img.BalancedFileKeys, [30,10,20;10,30,20;20,20,20]', .78, .22);
-% 
-% % generate trials - 30 images
-% [blocks,inputArgs,parsedResults]=generateEthBlocks(img.BalancedFileKeys, [15,5,10;5,15,10;10,10,10]', .78, .22);
-% save(fullfile(output_folder, 'contrast_30images_a.mat'), 'blocks', 'inputArgs', 'parsedResults');
-% 
-% save(NAME_HERE, 'blocks', 'inputArgs', 'parsedResults');
+    blockTypes = {'left', 'right', 'none'};
+    if ~ischar(identifier) || ~any(ismember(blockTypes, lower(lrn)))
+        error('expecting char inputs');
+    end
+    blockType = blockTypes(ismember(blockTypes, lower(lrn)));
 
-% Screen
-PsychDefaultSetup(2);
-%[w,wr]=PsychImaging('OpenWindow', 1, [.5 .5 .5]');
+    % Set these folders according to the current machine
+    image_folder = '/data/cclab/images/Babies';
+    input_folder = '/home/dan/git/cclab_ethosal/input';
+    output_folder = '/home/dan/git/cclab_ethosal/output';
+
+    outputFilename = fullfile(output_folder, [identifier, '_', blockType{1}, '.mat']);
+    if isfile(outputFilename)
+        warning('OutputFile %s already exists. Finding a suitable name...', outputFilename);
+        [path, base, ext] = fileparts(outputFilename);
+        [ok, outputFilename] = makeNNNFilename(fullfile(path, [base, '_NNN', ext]));
+        if ~ok
+            error('Cannot form usable filename using folder %s and basename %s', p.Results.Folder, [base, '_NNN', ext]);
+        end
+    end
+    fprintf('\n*** Using output filename %s\n', outputFilename);
 
 
-
-% load imageset
-img=imageset('/data/cclab/images/Babies', 'Subfolders', {'H', 'bw'; 'L', 'bw-texture'}, 'OnLoad', @deal);
-cc=load('/home/cclab/Desktop/ethosal/input/contrast_60images_a.mat');
-
-
-% parameters needed
-mkind = cclabGetMilliKeyIndices();
-kbind = getKeyboardIndex("Dell KB216 Wired Keyboard");
-kbind = getKeyboardIndex("Dell Dell USB Keyboard");
-screen_dimensions=[598, 336];
-screen_distance=1000;
-dummy_mode=0;   % 0 for participant
-
-results=etholog(cc.blocks{1}, img, screen_dimensions, screen_distance, ...
-    'ImageChangeType', 'contrast', ...
-    'Screen', 1, ...
-    'Out', output_folder, ...
-    'Response', 'MilliKey', ...
-    'MilliKeyIndex', mkind, ...
-    'KeyboardIndex', kbind, ...
-    'Beep', true, ...
-    'EyelinkDummyMode', dummy_mode, ...
-    'SkipSyncTests', 1);
-
-%save('/home/cclab/Desktop/ethosal/output/jodi-240-a.mat', 'results');
+    % load imageset
+    img=imageset(image_folder, 'Subfolders', {'H', 'bw'; 'L', 'bw-texture'}, 'OnLoad', @deal);
+    
+    % load trial blocks
+    cc=load(fullfile(input_folder, 'contrast_60images_a.mat'));
+    
+    % Millikey index (todo - test!)
+    mkind = cclabGetMilliKeyIndices();
+    
+    % keyboard index (todo - test)
+    
+    % This is the keyboard in use at the booth
+    kbind = getKeyboardIndex("Dell KB216 Wired Keyboard");
+    %kbind = getKeyboardIndex("Dell Dell USB Keyboard");
+    
+    screen_dimensions=[598, 336];
+    screen_distance=1000;
+    dummy_mode=0;   % 0 for participant, 1 for dummy mode
+        
+    
+    % % generate trials - 60 images
+    % [blocks,inputArgs,parsedResults]=generateEthBlocks(img.BalancedFileKeys, [30,10,20;10,30,20;20,20,20]', .78, .22);
+    % 
+    % % generate trials - 30 images
+    % [blocks,inputArgs,parsedResults]=generateEthBlocks(img.BalancedFileKeys, [15,5,10;5,15,10;10,10,10]', .78, .22);
+    % save(fullfile(output_folder, 'contrast_30images_a.mat'), 'blocks', 'inputArgs', 'parsedResults');
+    % 
+    % save(NAME_HERE, 'blocks', 'inputArgs', 'parsedResults');
+    
+    % Screen
+    %PsychDefaultSetup(2);
+    %[w,wr]=PsychImaging('OpenWindow', 1, [.5 .5 .5]');
+    
+    
+    
+    
+    
+    results=etholog(cc.blocks{indx}, img, screen_dimensions, screen_distance, ...
+        'ImageChangeType', 'contrast', ...
+        'Screen', 1, ...
+        'Out', output_folder, ...
+        'Response', 'MilliKey', ...
+        'MilliKeyIndex', mkind, ...
+        'KeyboardIndex', kbind, ...
+        'Beep', true, ...
+        'EyelinkDummyMode', dummy_mode, ...
+        'SkipSyncTests', 1);
+    
+    %save('/home/cclab/Desktop/ethosal/output/jodi-240-a.mat', 'results');
