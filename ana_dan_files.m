98cb1bb7b96155323a2388f63b6dfe13ba9a0936
Daniel Sperka <djsperka@ucdavis.edu>
2024-05-29 12:35:34 -0700

    add new files

--- ana_dan_files.m
+++ ana_dan_files.m
@@ -15,3 +15,8 @@ None=load('/home/dan/work/cclab/ethdata/output/taylor_none.mat');
 
 None=load('/home/dan/work/cclab/ethdata/output/xmch_none.mat');
 [rNone4, rAttended4] = anaeth([], [], None.results, 'xmch short sample');
+
+Left=load('/home/dan/work/cclab/ethdata/output/subject001_left.mat');
+Right=load('/home/dan/work/cclab/ethdata/output/subject001_right.mat');
+None=load('/home/dan/work/cclab/ethdata/output/subject001_none.mat');
+[rNone5, rAttended5] = anaeth(Left.results, Right.results, None.results, 's001');
