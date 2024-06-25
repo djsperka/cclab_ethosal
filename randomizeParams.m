5a6a9d6dfc8f0c20860b8027d32bb9d8bd9c7f7b
Daniel Sperka <djsperka@ucdavis.edu>
2024-06-25 14:48:01 -0700

    Remove Multiplicities parameter. This is now inferred from the structure of Replacements

--- randomizeParams.m
+++ randomizeParams.m
@@ -1,15 +1,34 @@
 function [t] = randomizeParams(varargin)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%randomizeParams Generate a table randomized over a set of parameters. 0
+%   Each of the first parameters is combined with each of the second, each
+%   of which is combined with the next parameter, and so on. The cell
+%   array reps determines the order and multiplicity at each step. 
+%
+%   e.g. 
+%   numImages = 30;
+%   imageIndices = randperm(imgset.BalancedFileKeys(1:100), numImages);
+%   reps{1} = imgset.BalancedFileKeys(imageIndices);
+
     
     p = inputParser;
-    p.addRequired('Multiplicities', @(x) isnumeric(x) && isvector(x));
-    p.addParameter('VariableNames', {}, @(x) iscell(x) && (length(x)==0 || length(x)==length(p.Results.Multiplicities)));
-    p.addParameter('Replacements', {}, @(x) iscell(x) && (length(x)==0 || length(x)==length(p.Results.Multiplicities)));
+    %p.addOptional('Multiplicities',[],@(x) isnumeric(x) && isvector(x));
+    %p.addRequired('Multiplicities', @(x) isnumeric(x) && isvector(x));
+    p.addParameter('VariableNames', {}, @(x) iscell(x));
+    p.addParameter('Replacements', {}, @(x) iscell(x));
     p.parse(varargin{:});
 
+    % should check params more carefully
+    if ~isempty(p.Results.VariableNames) && ~isempty(p.Results.Replacements)
+        if size(p.Results.VariableNames, 1) ~= size(p.Results.Replacements, 1)
+            error('VariableNames and Replacements should have same number of columns');
+        end
+    end
+
     t = [];
-    m = p.Results.Multiplicities;
+
+    % get multiplicities from the size of first dim of each element (the
+    % height of each one).
+    m = cellfun(@(x) size(x,1), p.Results.Replacements);
     n = prod(m);
     
     % generate list of values. Each value is a comb of params.
@@ -41,6 +60,7 @@ function [t] = randomizeParams(varargin)
         t{:,i} = A;
     end
     if ~isempty(p.Results.VariableNames)
+        p.Results.VariableNames
         t.Properties.VariableNames = p.Results.VariableNames;
     end
 %     t = array2table(pind);
