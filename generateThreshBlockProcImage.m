0fd0d17cf84b57b0556464a531e1999e6d258d66
Daniel Sperka <djsperka@ucdavis.edu>
2024-06-25 14:46:58 -0700

    Initial add

---
+++ generateThreshBlockProcImage.m
@@ -0,0 +1,119 @@
+function trialsOrBlocks = generateThreshBlockProcImage(varargin)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+    p=inputParser;  
+    p.addRequired('FileKeys', @(x) iscellstr(x));    
+    p.addRequired('NumImages', @(x) isnumeric(x) && isscalar(x));
+    %p.addRequired('Deltas', @(x) isnumeric(x) && isvector(x)); % TODO || (iscell(x) && all(cellfun(@(y) isnumeric(y) && isvector(y), x))));
+    p.addOptional('FixationTime', 0.5, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('MaxAcquisitionTime', 2.0, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('FixationBreakEarlyTime', 0.5, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('FixationBreakLateTime', 2.0, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('SampTime', 0.4, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('RespTime', 2.0, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('GapTime', 0.2, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('TestTime', 0.4, @(x) isnumeric(x) && length(x)<3);
+    p.addOptional('NumBlocks', 1, @(x) isnumeric(x) && x>0);
+
+    p.parse(varargin{:});
+
+    % build replacements arrays
+    reps = cell(4,1);
+    names=cell(4,1);
+
+    % Image key
+    imageIndices = randperm(length(p.Results.FileKeys), p.Results.NumImages);
+    reps{1} = reshape(p.Results.FileKeys(imageIndices), p.Results.NumImages, 1);
+    names{1} = 'ImageKey';
+
+    % salience
+    reps{2}={'H';'Q'};
+    names{2} = 'FolderKey';
+
+    % lr test
+    reps{3} = [1;2];
+    names{3} = 'StimTestType';
+
+    % delta
+    deltas = [-20;-10;0;10;20];
+    reps{4} = deltas;
+    names{4} = 'Delta';
+    trialsOrBlocks = randomizeParams('VariableNames', names, 'Replacements', reps);
+    nTrials = height(trialsOrBlocks);
+ 
+    % StimKeys
+    trialsOrBlocks.Stim1Key = imageset.make_keys(trialsOrBlocks.FolderKey, trialsOrBlocks.ImageKey);
+    
+    mH=containers.Map([-20,-10,0,10,20],{'F','G','H','I','J'});
+    mQ=containers.Map([-20,-10,0,10,20],{'O','P','Q','R','S'});
+    lzH = strcmp(trialsOrBlocks.FolderKey, 'H');
+    lzQ = strcmp(trialsOrBlocks.FolderKey, 'Q');
+    z = cell(height(trialsOrBlocks), 1);
+    z(lzH) = arrayfun(@(x) {mH(x)}, trialsOrBlocks.Delta(lzH));
+    z(lzQ) = arrayfun(@(x) {mQ(x)}, trialsOrBlocks.Delta(lzQ));
+    trialsOrBlocks.Stim2Key = imageset.make_keys(z, trialsOrBlocks.ImageKey);
+    
+%     % StimChangeType
+     trialsOrBlocks.StimChangeType = zeros(nTrials, 1);
+     trialsOrBlocks.StimChangeType(trialsOrBlocks.StimTestType==1 & trialsOrBlocks.Delta~=0) = 1;
+     trialsOrBlocks.StimChangeType(trialsOrBlocks.StimTestType==2 & trialsOrBlocks.Delta~=0) = 2;
+%     
+%     % base value
+%     trialsOrBlocks.Base = generateColumn(nTrials, 100);
+
+    % etc
+    trialsOrBlocks.FixationTime = generateColumn(nTrials, p.Results.FixationTime);
+    trialsOrBlocks.MaxAcquisitionTime = generateColumn(nTrials, p.Results.MaxAcquisitionTime);
+    trialsOrBlocks.FixationBreakEarlyTime = generateColumn(nTrials, p.Results.FixationBreakEarlyTime);
+    trialsOrBlocks.FixationBreakLateTime = generateColumn(nTrials, p.Results.FixationBreakLateTime);
+    trialsOrBlocks.SampTime = generateColumn(nTrials, p.Results.SampTime);
+    trialsOrBlocks.GapTime = generateColumn(nTrials, p.Results.GapTime);
+    trialsOrBlocks.RespTime = generateColumn(nTrials, p.Results.RespTime);
+    trialsOrBlocks.TestTime = generateColumn(nTrials, p.Results.TestTime);
+
+    % results
+    trialsOrBlocks.Started = false(nTrials, 1);
+    trialsOrBlocks.trialIndex = zeros(nTrials, 1);
+    trialsOrBlocks.Aon = generateColumn(nTrials, -1);
+    trialsOrBlocks.Aoff = generateColumn(nTrials, -1);
+    trialsOrBlocks.Bon = generateColumn(nTrials, -1);
+    trialsOrBlocks.Boff = generateColumn(nTrials, -1);
+    trialsOrBlocks.tResp = generateColumn(nTrials, -1);
+    trialsOrBlocks.iResp = -1*ones(nTrials, 1);
+
+
+    % checkif we need to break into blocks
+    if p.Results.NumBlocks > 1
+        % I want to make sure there's no rounding error on the last one, 
+        % so I force the last element to be the height.
+        endIndex = round(cumsum(ones(1, p.Results.NumBlocks)/p.Results.NumBlocks) * nTrials);
+        endIndex(p.Results.NumBlocks) = nTrials;    
+        blocks = cell(p.Results.NumBlocks, 1);
+        lastEnd = 0;
+        for iblock=1:p.Results.NumBlocks
+            blocks{iblock} = trialsOrBlocks(lastEnd+1:endIndex(iblock), :);
+            fprintf('Block %d has %d elements\n', iblock, height(blocks{iblock}));
+            lastEnd = endIndex(iblock);
+        end
+        trialsOrBlocks = blocks;
+    end
+
+
+
+end
+
+
+function [A] = generateColumn(n, valueOrRange)
+% generateColumn - generate a column of n values. If 'valueOrRange' is
+% scalar, all n values are set to it. If 'valueOrRange' is a two-element
+% vector, it should be a range within which the values should vall - they
+% are drawn from a uniform distribtion.
+
+    if length(valueOrRange)==1
+        A = valueOrRange * ones(n, 1);
+    else
+        A = valueOrRange(1) + (valueOrRange(2)-valueOrRange(1)) * rand(n, 1);
+    end
+end
+
