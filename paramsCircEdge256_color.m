fb592c19e52ea1f452643fbce07b5320f07c0bc8
Daniel Sperka <djsperka@ucdavis.edu>
2025-04-16 11:38:12 -0700

    Update folder names for the bw images. Must also update cclab-images to tag Update2.

--- paramsCircEdge256_color.m
+++ paramsCircEdge256_color.m
@@ -1,6 +1,6 @@
 function Y = paramsCircEdge256_color()
     Y.Subfolders={ ...
-    'H','nat';'L','tex';'C','color';'D','color-tex'
+    'H','bw';'L','bw-tex';'C','color';'D','color-tex'
     };
     Y.MaskParameters = [256,128,100,100];
     %Y.OnLoadFunc=@(x) imresize(x,[256,256]);
