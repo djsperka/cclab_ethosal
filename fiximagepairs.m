b3c6224f1fd9800d8519b9402a7e383ef281dc1f
Daniel Sperka <djsperka@ucdavis.edu>
2026-07-06 16:47:14 -0700

    added more fixes

--- fiximagepairs.m
+++ fiximagepairs.m
@@ -1,16 +1,28 @@
 % load blockset
-Y=load("/home/dan/work/cclab/ethdata/input/rimg_exp_ColorCue-30-10-ALL.mat");
-N=size(Y.blockset, 1);
+Y=load("C:/Users/cclab/Desktop/work/data/input/rimg_exp_60-20-left-A.mat");
+%N=size(Y.blockset, 1);
+N=8;
 V=[];
 
 % loop over blocks. Get all imagepairindices, along with their file keys. 
 % Convert filekeys to integers to store in an array with image pair index.
+% for i=1:N
+%     IPI = Y.blockset(i).trials.ImagePairIndex;
+%     F = Y.blockset(i).trials.File1Key;
+%     FN = cellfun(@(x) str2num(x), F);
+%     V=vertcat(V,[IPI,FN]);
+%     size(V)
+% end
+
 for i=1:N
-    IPI = Y.blockset(i).trials.ImagePairIndex;
-    F = Y.blockset(i).trials.File1Key;
+    IPI = Y.blocks{i}.ImagePairIndex;
+    F = Y.blocks{i}.File1Key;
     FN = cellfun(@(x) str2num(x), F);
     V=vertcat(V,[IPI,FN]);
+    size(V)
 end
+
+
 
 % sort on image pair index column (1)
 Vsorted = sortrows(V);
